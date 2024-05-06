772435265f6bb9be6d38be11667bf704ea306e71
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-05-06 17:18:40 +0900

    5월6일 타입스크립트 강의 완

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props)=>props.theme.backgroundColor};
+const Container = styled.div`
+  background-color: ${(props)=> props.theme.bgColor};
 `;
-
-const Text = styled.h1`
-  font-size: 40px;
+const H1 = styled.h1`
   color: ${(props)=> props.theme.textColor};
 `
 
-
 function App() {
   return(
-    <Wrapper>
-     <Text>Hello</Text>
-    </Wrapper>
+    <Container>
+      <H1> Hello </H1>
+    </Container>
   )
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from 'styled-components'
 import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from './theme.ts'
 
-const darkTheme = {
-  textColor:"whitesmoke",
-  backgroundColor:"#111"
-}
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke"
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
